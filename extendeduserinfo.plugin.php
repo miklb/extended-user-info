c4afa685b87afc0c526c33fab100a8b974ff891f
Michael Bishop <miklb@miklb.com>
2012-06-20 02:35:32 -0400

    initial commit of plugin file

---
+++ extendeduserinfo.plugin.php
@@ -0,0 +1,28 @@
+<?php if ( !defined( 'HABARI_PATH' ) ) { die( 'No direct access' ); }
+
+class ExtendedUserInfo extends Plugin
+{
+	public function action_form_user ($form, $user) {
+
+		/**
+	 	* Add the configuration to the user page 
+	 	**/
+
+		//Add Twitter URL
+		$twitterurl = $form->user_info->append( 'text', 'twitterurl', 'null:null', _t( 'Twitter URL' ), 'optionscontrol_text' );
+		$twitterurl->class[] = 'item clear';
+		$twitterurl->value = $user->info->twitterurl;
+
+		//Add Facebook URL
+		$facebookurl = $form->user_info->append( 'text', 'facebookurl', 'null:null', _t('Facebook URL' ), 'optionscontrol_text' );
+		$facebookurl->class[] = 'item clear';
+		$facebookurl->value = $user->info->facebookurl;
+
+		//Add Bio blurb
+		$bio = $form->user_info->append ('textarea', 'bioblurb', 'null:null', _t('Bio Blurb'), 'optionscontrol_text' );
+		$bio->class[] = 'item clear';
+		$bio->value = $user->info->bioblurb;
+
+	}
+}
+?>
